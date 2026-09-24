0a925e89663f6098e97367f816481d49d2e9e257
Spencer <mspencer@catalyte.io>
2026-09-24 16:10:06 -0400

    updated documenation and removed the address attribute

--- main.py
+++ main.py
@@ -3,10 +3,9 @@
 status = ['Applied', 'Accepted', 'Active', 'Completed', 'Dismissed', 'Rejected']
 
 class Applicant:
-    def __init__(self,name,age,address,street,city,state,zip,status):
+    def __init__(self,name,age,street,city,state,zip,status):
         self.name = name
         self.age = age
-        self.address = address
         self.street = street
         self.city = city
         self.state = state
@@ -19,8 +18,7 @@ class Applicant:
                 
         
             Returns:
-                Bool: Indicates whether the applicant is greater than 18.
-            
+                Bool: Indicates whether the applicant is greater than 18.   
         """  
         if self.age > 18:
             return True
@@ -30,7 +28,7 @@ class Applicant:
     def is_active_applicant(self):
         """Takes an applicant as a parameter and returns
                 a boolean that indicates whether or not the applicant's 
-                program status is "Active
+                program status is "Active"
             
                 Returns:
                     Bool: Indicates whether the applicant is active in the program.
@@ -41,16 +39,16 @@ class Applicant:
         else:
             return False
 
-applicant_01 = Applicant("Abigail Fontane", 44, "123", "Gibby St","Havalina", "NY", "94949", status[2])
-applicant_02 = Applicant("Bruce Lee", 52, "1343 ", "IDK Ave", "Greer", "MO", "96969",status[1])
-applicant_03 = Applicant("Charlie Foster", 13, "3343 ", "Hall Ln", "Nantucket", "NC", "84549", status[5])
-applicant_04 = Applicant("Dean Withers", 16, "984 ", "Pace Ave", "Franklin", "IL", "07552", status[5])
-applicant_05 = Applicant("Esteban Zanzabar", 18, "777", "Luck St", "Foster", "NY", "20209", status[4])
-applicant_06 = Applicant("Florence Machine", 19, "656", "Calumm Dr", "Newark", "DE", "08312", status[2])
-applicant_07 = Applicant("Giana Ovaltine", 99, "7535", "Not Way", "Wheaton", "MD", "02265", status[2])
-applicant_08 = Applicant("Rock T. Cazbah", 73, "244", "Heart St", "Kensington", "CA", "84521", status[3])
-applicant_09 = Applicant("Imogen Heap", 21, "782", "Boop Pl", "Haplern", "FL", "59643", status[4])
-applicant_10 = Applicant("Jackie Robinson", 32, "789", "Funt Dr", "Greenbelt", "GA", "12548", status[1])
+applicant_01 = Applicant("Abigail Fontane", 44, "123 Gibby St","Havalina", "NY", "94949", status[2])
+applicant_02 = Applicant("Bruce Lee", 52, "1343 IDK Ave", "Greer", "MO", "96969",status[1])
+applicant_03 = Applicant("Charlie Foster", 13, "3343 Hall Ln", "Nantucket", "NC", "84549", status[5])
+applicant_04 = Applicant("Dean Withers", 16, "984 Pace Ave", "Franklin", "IL", "07552", status[5])
+applicant_05 = Applicant("Esteban Zanzabar", 18, "777 Luck St", "Foster", "NY", "20209", status[4])
+applicant_06 = Applicant("Florence Machine", 19, "656 Calumm Dr", "Newark", "DE", "08312", status[2])
+applicant_07 = Applicant("Giana Ovaltine", 99, "7535 Not Way", "Wheaton", "MD", "02265", status[2])
+applicant_08 = Applicant("Rock T. Cazbah", 73, "244 Heart St", "Kensington", "CA", "84521", status[3])
+applicant_09 = Applicant("Imogen Heap", 21, "782 Boop Pl", "Haplern", "FL", "59643", status[4])
+applicant_10 = Applicant("Jackie Robinson", 32, "789 Funct Dr", "Greenbelt", "GA", "12548", status[1])
 
 applicant_fleet = [applicant_01,applicant_02,applicant_03,applicant_04,applicant_05,applicant_06,applicant_07,
               applicant_08,applicant_09,applicant_10]
@@ -59,9 +57,14 @@ class ApplicantTracker:
     def __init__(self, applicants):
         self.applicants = list(applicants)
 
-
+ 
     def filter_applicants_by_eligibility(self):
-        """Returns a list of eligible applicants.
+        """
+            Filters out applicants whose age meet the over 18 requirement.
+                            
+            Returns:
+               list: A list of applicant objects   
+                
         """
         eligible_list = []
         for applicant in self.applicants:
@@ -72,8 +75,12 @@ class ApplicantTracker:
         return eligible_list
 
     def filter_active_applicants(self):
-        """Returns a list of Applicant objects 
-            currently in an "Active" status."""
+        """
+            Filters out applicants that have a status of "Active" and adds them to a list.
+        
+            Returns:
+                list: A list of Applicant objects
+        """
         active_list = []
         for applicant in self.applicants:
             if applicant.is_active_applicant():
@@ -83,8 +90,13 @@ class ApplicantTracker:
         return active_list
 
     def report_completed_applicants(self):
-        """Returns the total count (integer) of 
-            all applicants whose status is "Completed"."""
+        """
+            Counts the number of applicants with a satus of completed.
+            
+            Returns:
+                list: A list of Applicant objects
+            
+            """
         completed_list = []
         for applicant in self.applicants:
             if applicant.status == "Completed":
